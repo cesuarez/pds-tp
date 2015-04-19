3cb3a87cb8633c525f930a5f2994ffa7e4b05a43
Cristian Suarez <cris.eze.sua@gmail.com>
2015-04-19 18:25:07 -0300

    Sacado un if que me olvide

--- tp/public/javascripts/controllers/posts_ctrl.js
+++ tp/public/javascripts/controllers/posts_ctrl.js
@@ -2,47 +2,44 @@ angular.module('postsApp').controller('PostsCtrl',
     ['$scope', '$stateParams', 'postsFactory', 'post', '$location', function(
     $scope, $stateParams, postsFactory, post, $location){
 
-    if ($stateParams.id){
-        $scope.post = post;
-        $scope.customFilter = '-upvotes';
-        updateTimes();
+    $scope.post = post;
+    $scope.customFilter = '-upvotes';
+    updateTimes();
 
-        $scope.addComment = function(){
-            if($scope.body !== '' && $scope.body !== undefined) {
-                postsFactory.addComment(post._id, {
-                    body: $scope.body,
-                    author: ($scope.author ? $scope.author : 'Anonymous'),
-                    time: moment().toDate()
-                    }).success(function(comment) {
-                        $scope.post.comments.push(comment);
-                        updateTimes();
-                    });
-                $scope.body = '';
-                $scope.author = '';
-            }
-        };
-
-        $scope.incrementUpvotes = function(comment){
-            postsFactory.upvoteComment(post, comment);
-        };
-
-        $scope.back = function(){
-            $location.url("");
+    $scope.addComment = function(){
+        if($scope.body !== '' && $scope.body !== undefined) {
+            postsFactory.addComment(post._id, {
+                body: $scope.body,
+                author: ($scope.author ? $scope.author : 'Anonymous'),
+                time: moment().toDate()
+                }).success(function(comment) {
+                    $scope.post.comments.push(comment);
+                    updateTimes();
+                });
+            $scope.body = '';
+            $scope.author = '';
         }
+    };
 
-        function updateTimes(){
-            angular.forEach($scope.post.comments, function(value){
-                value.time = (moment(value.time));
-            });
-        }
+    $scope.incrementUpvotes = function(comment){
+        postsFactory.upvoteComment(post, comment);
+    };
 
-        // Dynamic Ordering
-        $scope.order = "-upvotes";
+    $scope.back = function(){
+        $location.url("");
+    }
 
-        $scope.changeOrderTo = function(order) {
-            $scope.order = order;
-        }
+    function updateTimes(){
+        angular.forEach($scope.post.comments, function(value){
+            value.time = (moment(value.time));
+        });
+    }
 
+    // Dynamic Ordering
+    $scope.order = "-upvotes";
+
+    $scope.changeOrderTo = function(order) {
+        $scope.order = order;
     }
 
 }]);
