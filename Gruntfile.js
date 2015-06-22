0627caf194aa754adf3fd4a450f7c059ebf6bc36
Cristian Suarez <cris.eze.sua@gmail.com>
2015-06-22 00:15:20 -0300

    travis fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       travis: {
           configFile: 'karma.conf.js',
           singleRun: true,
-          browsers: ['PhantomJS']
+          browsers: ['Firefox']
       }
     },
 
